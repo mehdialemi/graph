f679b151a7fedf317da2d968de77781006d5bf9a
mehdi alemi <mehdi.alemi.se@gmail.com>
2016-08-30 04:08:24 +0430

    1

--- src/main/java/ir/ac/sbu/graph/RedisTest.java
+++ src/main/java/ir/ac/sbu/graph/RedisTest.java
@@ -1,6 +1,7 @@
 package ir.ac.sbu.graph;
 
 import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisURI;
 import com.lambdaworks.redis.api.StatefulRedisConnection;
 
 /**
@@ -9,7 +10,7 @@ import com.lambdaworks.redis.api.StatefulRedisConnection;
 public class RedisTest {
 
     public static void main(String[] args) {
-        RedisClient redisClient = RedisClient.create(args[0]);
+        RedisClient redisClient = RedisClient.create(RedisURI.create("redis://localhost:6379/0"));
         StatefulRedisConnection<String, String> connection = redisClient.connect();
         connection.async().incr("10:10");
 
