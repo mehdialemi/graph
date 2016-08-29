a53c2645bbd4f1729b7639b37cac29d1ee27038b
mehdi alemi <mehdi.alemi.se@gmail.com>
2016-08-30 03:41:52 +0430

    Redis

--- src/main/java/ir/ac/sbu/graph/RedisTest.java
+++ src/main/java/ir/ac/sbu/graph/RedisTest.java
@@ -1,7 +1,19 @@
 package ir.ac.sbu.graph;
 
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.api.StatefulRedisConnection;
+
 /**
- * Created by mehdi on 8/30/16.
+ *
  */
 public class RedisTest {
+
+    public static void main(String[] args) {
+        RedisClient redisClient = RedisClient.create(args[0]);
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        connection.async().incr("10:10");
+
+        connection.close();
+        redisClient.shutdown();
+    }
 }
