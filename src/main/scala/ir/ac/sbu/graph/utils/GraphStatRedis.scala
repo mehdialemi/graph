ca3c31e3a14b94f7d85c6d08146b453063bcf321
mehdi alemi <mehdi.alemi.se@gmail.com>
2016-08-30 21:22:54 +0430

    x

--- src/main/scala/ir/ac/sbu/graph/utils/GraphStatRedis.scala
+++ src/main/scala/ir/ac/sbu/graph/utils/GraphStatRedis.scala
@@ -3,7 +3,6 @@ package ir.ac.sbu.graph.utils
 import ir.ac.sbu.graph.GraphUtils
 import ir.ac.sbu.redispark.{RedisContext, RedisEndpoint}
 import org.apache.spark.{SparkConf, SparkContext}
-import redis.clients.jedis.Jedis
 
 /**
   * Created by mehdi on 8/30/16.
@@ -23,9 +22,6 @@ object GraphStatRedis {
         if (args == null || args.length == 0)
             conf.setMaster("local[2]")
 
-//        val jedis = new Jedis("malemi-2", 6379, 60000, true)
-//        val pipline = jedis.pipelined()
-
         GraphUtils.setAppName(conf, "Graph-Stat-By-Redis", partition, inputPath);
         conf.set("redis.host", "malemi-2").set("redis.port", "6379")
 
@@ -33,14 +29,7 @@ object GraphStatRedis {
         val edges = sc.textFile(inputPath, partition)
           .filter(t => !t.startsWith("#")).map(t => t.split("\\s+"))
           .map(t => t(0).toLong -> t(1).toLong)
-//
-//        val count = edges.map(t => {
-//            pipline.incr(t._1.toString)
-//            pipline.incr(t._2.toString)
-//            t._1
-//        }).count()
 
-//        println(count)
         val rc = new RedisContext(sc)
 
         val c = rc.incr(edges)(new RedisEndpoint("malemi-2", 6379)).count()
