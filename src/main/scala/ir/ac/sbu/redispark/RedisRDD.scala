5e358c51612a3258693ee109b8947dd97bbcc877
mehdi alemi <mehdi.alemi.se@gmail.com>
2016-08-30 14:31:22 +0430

    1

--- src/main/scala/ir/ac/sbu/redispark/RedisRDD.scala
+++ src/main/scala/ir/ac/sbu/redispark/RedisRDD.scala
@@ -3,6 +3,7 @@ package ir.ac.sbu.redispark
 import org.apache.spark.{Partition, TaskContext}
 import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.rdd.RDD
+import redis.clients.jedis.Jedis
 
 /**
   * Created by mehdi on 8/30/16.
@@ -11,20 +12,15 @@ class RedisRDD(rdds: RDD[(Long, Long)], redisEndpoint: RedisEndpoint) extends RD
 
     @DeveloperApi
     override def compute(split: Partition, context: TaskContext): Iterator[(Long, Long)] = {
-        val pool = new ConnectionPool(redisEndpoint.host, redisEndpoint.port);
-
+        val jedis = new Jedis(redisEndpoint.host, redisEndpoint.port)
         rdds.iterator(split, context).foreach{
             x => {
-                var jedis = pool.getJedis();
                 jedis.incr(x._1.toString)
-                jedis.close()
 
-                jedis = pool.getJedis();
                 jedis.incr(x._2.toString)
-                jedis.close()
             }
         }
-        pool.close()
+        jedis.close()
         Iterator()
     }
 
