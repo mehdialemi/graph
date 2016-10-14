4189eb81a0b45fe9023dcd27bcfb9b825c86a241
mehdi alemi <mehdi.alemi.se@gmail.com>
2016-10-15 03:18:42 +0330

    script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+git checkout .
 git checkout -- target/subgraph-mining-1.0.jar
 git checkout -- target/subgraph-mining-1.0-jar-with-dependencies.jar
 git pull --rebase origin master
