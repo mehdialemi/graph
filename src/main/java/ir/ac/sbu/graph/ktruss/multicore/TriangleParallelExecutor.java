6097a26a6e526aff2e69e136f7792dbf49946461
mehdi alemi <mehdi.alemi.se@gmail.com>
2016-11-11 10:13:48 +0330

    There are some bugs but parallelism is changed in triangle counting

--- src/main/java/ir/ac/sbu/graph/ktruss/multicore/TriangleParallelExecutor.java
+++ src/main/java/ir/ac/sbu/graph/ktruss/multicore/TriangleParallelExecutor.java
@@ -21,8 +21,8 @@ public class TriangleParallelExecutor {
         ExecutorService executorService = Executors.newFixedThreadPool(threads);
         // find max vertex Id in parallel
         long t1 = System.currentTimeMillis();
-        List<Tuple2<Integer, Integer>> buckets = createBuckets(threads, edges.length);
-        int max = buckets.parallelStream()
+        final List<Tuple2<Integer, Integer>> edgeBuckets = createBuckets(threads, edges.length);
+        int max = edgeBuckets.parallelStream()
             .map(bucket -> Arrays.stream(edges, bucket._1, bucket._2)
                 .map(e -> Math.max(e.v1, e.v2))
                 .reduce((v1, v2) -> Math.max(v1, v2)).orElse(-1))
@@ -35,27 +35,23 @@ public class TriangleParallelExecutor {
 
         // construct deg array
         AtomicInteger[] degArray = new AtomicInteger[max + 1];
-        buckets = createBuckets(threads, degArray.length);
-        buckets.parallelStream().forEach(bucket ->
-            IntStream.range(bucket._1, bucket._2).forEach(i -> degArray[i] = new AtomicInteger(0)));
+        IntStream.range(0, degArray.length).parallel().forEach(i -> degArray[i] = new AtomicInteger(0));
+
         long t3 = System.currentTimeMillis();
         System.out.println("Construct degArray (AtomicInteger) in " + (t3 - t2) + " ms");
 
         // Construct degree array such that vertexId is the index of the array in parallel
-        buckets = createBuckets(threads, edges.length);
-        buckets.parallelStream().forEach(bucket ->
-            Arrays.stream(edges, bucket._1, bucket._2).forEach(e -> {
+        Arrays.stream(edges).parallel().forEach(e -> {
                 degArray[e.v1].incrementAndGet();
                 degArray[e.v2].incrementAndGet();
-            })
-        );
+        });
 
         long t4 = System.currentTimeMillis();
         System.out.println("Fill degArray in " + (t4 - t3) + " ms");
 
         // Fill and sort vertices array.
-        int[] vertices = TriangleParallel.sort(degArray, threads);
-        int[] rvertices = new int[vertices.length];
+        final int[] vertices = TriangleParallel.sort(degArray, threads);
+        final int[] rvertices = new int[vertices.length];
         IntStream.range(0, threads).forEach(index -> {
             for (int i = index; i < vertices.length; i += threads)
                 rvertices[vertices[i]] = i;
@@ -65,19 +61,14 @@ public class TriangleParallelExecutor {
         System.out.println("Sort degArray in " + (t5 - t4) + " ms");
 
         // Construct neighborhood
-        List<Long>[] neighbors = new List[vertices.length];
-        List<Tuple2<Integer, Integer>> veBucket = createBuckets(threads, vertices.length);
-        veBucket.parallelStream().forEach(bucket ->
-            Arrays.stream(vertices, bucket._1, bucket._2).forEach(v ->
-                neighbors[v] = new ArrayList()
-            )
-        );
+        final List<Long>[] neighbors = new List[vertices.length];
+        Arrays.stream(vertices).parallel().forEach(v -> neighbors[v] = new ArrayList<>());
+
         long t6 = System.currentTimeMillis();
         System.out.println("Construct neighbors in " + (t6 - t5) + " ms");
 
-        buckets = MultiCoreUtils.createBuckets(threads, edges.length);
         // Fill neighbors array
-        buckets.parallelStream().forEach(bucket -> {
+        edgeBuckets.parallelStream().forEach(bucket -> {
             for (int i = bucket._1; i < bucket._2; i++) {
                 Edge e = edges[i];
                 if (rvertices[e.v2] >= rvertices[e.v1]) {
@@ -91,32 +82,38 @@ public class TriangleParallelExecutor {
                 }
             }
         });
+
+        //TODO sort neighbor list???
+
         long t7 = System.currentTimeMillis();
         System.out.println("Fill neighbors in " + (t7 - t6) + " ms");
         long t8 = System.currentTimeMillis();
 
         AtomicInteger triangleOffset = new AtomicInteger(0);
-        final int offset = (int) Arrays.stream(degArray).parallel().filter(d -> d.get() < 2).count();
-        final List<Tuple2<Integer, Integer>> bucketsIndex = createBuckets(threads, offset, vertices.length);
         long t9 = System.currentTimeMillis();
-        System.out.println("Ready to triangle in " + (t9 - t8) + " ms with " + buckets.size() + " buckets");
+        System.out.println("Ready to triangle in " + (t9 - t8) + " ms with " + edgeBuckets.size() + " edgeBuckets");
 
         List<Future<Tuple2<Map<Integer, int[]>, Map<Integer, List<Integer>>>>> futures = new ArrayList<>();
         ArrayBlockingQueue<Integer> completedThreadsQueue = new ArrayBlockingQueue<>(threads);
         final AtomicInteger maxTriangles = new AtomicInteger(0);
         final Object lock = new Object();
+        final int offset = (int) Arrays.stream(degArray).parallel().filter(d -> d.get() < 2).count();
+        final int length = vertices.length;
+        final AtomicInteger blockOffset = new AtomicInteger(offset);
+
         for (int th = 0; th < threads; th++) {
             final int index = th;
             Future<Tuple2<Map<Integer, int[]>, Map<Integer, List<Integer>>>> future = executorService.submit(() -> {
                 Map<Integer, List<Integer>> localEdgeTriangles = new HashMap<>();
                 Map<Integer, int[]> localTriangles = new HashMap<>();
                 int triangleNum = triangleOffset.getAndAdd(BUCKET_COUNT);
-
-                for (int b = 0; b < bucketsIndex.size(); b++) {
-                    Tuple2<Integer, Integer> bucket = bucketsIndex.get(b);
-                    for (int i = index + bucket._1; i < bucket._2; i += threads) {
+                int i = 0;
+                while (i < length) {
+                    int start = blockOffset.getAndAdd(BUCKET_COUNT);
+                    for (i = start; i < start + BUCKET_COUNT && i < length; i++) {
                         int u = vertices[i];   // get current vertex id as u
 
+                        // TODO use bitset
                         // construct a map of nodes to their edges for u
                         final List<Long> uNeighbors = neighbors[u];
                         Map<Integer, Integer> unEdges = new HashMap<>(uNeighbors.size());
@@ -163,6 +160,7 @@ public class TriangleParallelExecutor {
                                     }
                                     list.add(triangleNum);
 
+
                                     localTriangles.put(triangleNum, new int[]{vw, uv, uw});
                                     triangleNum++;
                                     if (triangleNum % BUCKET_COUNT == 0)
@@ -176,6 +174,7 @@ public class TriangleParallelExecutor {
                 synchronized (lock) {
                     if (triangleNum > maxTriangles.get())
                         maxTriangles.set(triangleNum);
+                    System.out.println("Thread " + index + ", triangles: " + localTriangles.size());
                 }
                 completedThreadsQueue.add(index);
                 return new Tuple2<>(localTriangles, localEdgeTriangles);
@@ -184,7 +183,6 @@ public class TriangleParallelExecutor {
         }
 
         final HashSet<Integer>[] edgeTriangles = new HashSet[edges.length];
-        List<Tuple2<Integer, Integer>> edgeBuckets = MultiCoreUtils.createBuckets(threads, edges.length);
 
         int counter = 0;
         List<Map<Integer, int[]>> localTrianglesList = new ArrayList<>(threads);
@@ -195,21 +193,28 @@ public class TriangleParallelExecutor {
 
             final Tuple2<Map<Integer, int[]>, Map<Integer, List<Integer>>> result = futures.get(completedThread).get();
             localTrianglesList.add(result._1);
-            Map<Integer, List<Integer>> localEdgeTriangles = result._2;
-            edgeBuckets.parallelStream().forEach(eBucket -> {
-                for (int edgeIndex = eBucket._1; edgeIndex < eBucket._2; edgeIndex++) {
-                    List<Integer> et = localEdgeTriangles.get(edgeIndex);
-                    if (et != null) {
-                        if (edgeTriangles[edgeIndex] == null)
-                            edgeTriangles[edgeIndex] = new HashSet<>();
-                        edgeTriangles[edgeIndex].addAll(et);
-                    }
-                }
+            Set<Map.Entry<Integer, List<Integer>>> localEdgeTriangles = result._2.entrySet();
+
+            localEdgeTriangles.parallelStream().forEach(entry -> {
+                if (edgeTriangles[entry.getKey()] == null)
+                    edgeTriangles[entry.getKey()] = new HashSet<>();
+                edgeTriangles[entry.getKey()].addAll(entry.getValue());
             });
-
-
-            counter ++;
+//            edgeBuckets.parallelStream().forEach(eBucket -> {
+//                for (Map.Entry<Integer, List<Integer>> entry : localEdgeTriangles) {
+//                    Integer edgeIndex = entry.getKey();
+//                    if (edgeIndex >= eBucket._1 && edgeIndex < eBucket._2) {
+//                        if (edgeTriangles[edgeIndex] == null)
+//                            edgeTriangles[edgeIndex] = new HashSet<>();
+//                        edgeTriangles[edgeIndex].addAll(entry.getValue());
+//                    }
+//                }
+//            });
+            counter++;
         }
+
+        executorService.shutdown();
+
         int[][] triangles = new int[maxTriangles.get()][];
         localTrianglesList.parallelStream().forEach(localTriangles ->
             localTriangles.entrySet().forEach(triangle ->
