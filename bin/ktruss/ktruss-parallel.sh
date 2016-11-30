d6e9f822ba2b3a6ffc8a218a1c90a1612add4745
mehdi alemi <mehdi.alemi.se@gmail.com>
2016-11-30 19:53:00 +0330

    parallel method 3

--- bin/ktruss/ktruss-parallel.sh
+++ bin/ktruss/ktruss-parallel.sh
@@ -5,4 +5,7 @@ export BASEDIR=$(dirname "$0")
 
 echo "GC = $GC_OPTIONS"
 echo "JAR = $JAR_PATH"
-java $GC_OPTIONS -cp $JAR_PATH ir.ac.sbu.graph.ktruss.multicore.KTrussParallel "$HOME/graph-data/$INPUT" 5 6
+k=$1
+threads=$2
+method=$3
+java $GC_OPTIONS -cp $JAR_PATH ir.ac.sbu.graph.ktruss.parallel.KTrussParallel "$HOME/graph-data/$INPUT" $k $threads $method
