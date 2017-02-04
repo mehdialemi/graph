bf0be0e89602292bacd24da6a192fd3cda9de085
mehdi alemi <mehdi.alemi.se@gmail.com>
2017-02-05 03:16:30 +0330

    working KTrussSparkInvalidVertices

--- src/main/java/ir/ac/sbu/graph/ktruss/distributed/KTrussSparkInvalidVertices.java
+++ src/main/java/ir/ac/sbu/graph/ktruss/distributed/KTrussSparkInvalidVertices.java
@@ -16,9 +16,10 @@ import java.util.*;
 /**
  *
  */
-public class KTrussSparkTriangleJoin {
+public class KTrussSparkInvalidVertices {
 
     public static final int CO_PARTITION_FACTOR = 10;
+    public static final int[] ZERO_INT = new int[]{0};
 
     public static void main(String[] args) {
         String inputPath = "/home/mehdi/graph-data/com-amazon.ungraph.txt";
@@ -43,6 +44,8 @@ public class KTrussSparkTriangleJoin {
         conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        long tStart = System.currentTimeMillis();
+
         JavaRDD<String> input = sc.textFile(inputPath, partition);
 
         JavaPairRDD<Integer, Integer> edges = GraphLoader.loadEdgesInt(input);
@@ -51,7 +54,7 @@ public class KTrussSparkTriangleJoin {
 
         JavaPairRDD<Integer, int[]> candidates = FonlDegTC.generateCandidatesInteger(fonl);
 
-        JavaPairRDD<Tuple2<Integer, Integer>, IntSet> edgeVertexList = candidates.cogroup(fonl).flatMapToPair(t -> {
+        JavaPairRDD<Tuple2<Integer, Integer>, IntSet> edgeVertexes = candidates.cogroup(fonl).flatMapToPair(t -> {
             int[] fVal = t._2._2.iterator().next();
             Arrays.sort(fVal, 1, fVal.length);
             int v = t._1;
@@ -91,58 +94,80 @@ public class KTrussSparkTriangleJoin {
                 set.add(v.intValue());
             }
             return set;
-        });
+        }).repartition(partition).cache();
 
-        JavaPairRDD<Tuple2<Integer, Integer>, Integer> edgeSup = edgeVertexList.mapValues(v -> v.size()).cache();
+        JavaPairRDD<Tuple2<Integer, Integer>, int[]> edgeSup = edgeVertexes.mapValues(v -> new int[]{v.size()}).cache();
 
-        JavaPairRDD<Tuple2<Integer, Integer>, IntArraySet> invalidUpdates = sc.parallelize(new ArrayList<Integer>(), partition)
-            .mapToPair(f -> new Tuple2<>(new Tuple2<>(0, 0), new IntArraySet()));
+        JavaPairRDD<Tuple2<Integer, Integer>, int[]> prevEdgeSup = edgeSup;
         int iteration = 0;
         while (true) {
+            long t1 = System.currentTimeMillis();
             System.out.println("iteration: " + ++iteration);
-            JavaPairRDD<Tuple2<Integer, Integer>, Integer> invalids = edgeSup.filter(e -> e._2 < minSup).cache();
+            JavaPairRDD<Tuple2<Integer, Integer>, int[]> invalids = edgeSup.filter(value -> (value._2[0] + 1 - value._2.length) < minSup);
+
             long count = invalids.count();
-            System.out.println("invalid count: " + count);
+            long t2 = System.currentTimeMillis();
+            System.out.println("invalid count: " + count + ", duration: " + (t2 - t1) + " ms");
             if (count == 0)
                 break;
 
-            // Join invalids with  edgeVertexList
-            JavaPairRDD<Tuple2<Integer, Integer>, Iterable<Integer>> update =
-                invalids.join(edgeVertexList).mapValues(v -> v._2).flatMapToPair(kv -> {
-                List<Tuple2<Tuple2<Integer, Integer>, Integer>> out = new ArrayList<>(kv._2.size() * 2);
-                kv._2.stream().forEach(w -> {
-                    Integer u = kv._1._1;
-                    Integer v = kv._1._2;
-                    if (u < w)
-                        out.add(new Tuple2<>(new Tuple2<>(u, w), v));
-                    else
-                        out.add(new Tuple2<>(new Tuple2<>(w, u), v));
+            // Join invalids with  edgeVertexes
+            JavaPairRDD<Tuple2<Integer, Integer>, Iterable<Integer>> invalidUpdate =
+                invalids.join(edgeVertexes)
+                    .flatMapToPair(kv -> {
+                        for (int i = 1; i < kv._2._1.length; i++) {
+                            kv._2._2.remove(kv._2._1[i]);
+                        }
 
-                    if (v < w)
-                        out.add(new Tuple2<>(new Tuple2<>(v, w), u));
-                    else
-                        out.add(new Tuple2<>(new Tuple2<>(w, v), u));
-                });
-                return out.iterator();
-            }).groupByKey().subtractByKey(invalids);
+                        List<Tuple2<Tuple2<Integer, Integer>, Integer>> out = new ArrayList<>(kv._2._2.size() * 2);
 
-            edgeSup = update.rightOuterJoin(edgeSup).mapValues(values -> {
-                int sup = values._2;
-                if (!values._1.isPresent()) {
-                    if (sup < minSup)
-                        return 0;
-                    return sup;
+                        int u = kv._1._1;
+                        int v = kv._1._2;
+                        for (int w : kv._2._2) {
+                            if (u < w)
+                                out.add(new Tuple2<>(new Tuple2<>(u, w), v));
+                            else
+                                out.add(new Tuple2<>(new Tuple2<>(w, u), v));
+
+                            if (v < w)
+                                out.add(new Tuple2<>(new Tuple2<>(v, w), u));
+                            else
+                                out.add(new Tuple2<>(new Tuple2<>(w, v), u));
+                        }
+
+                        return out.iterator();
+                    }).groupByKey();
+
+            edgeSup = invalidUpdate.rightOuterJoin(edgeSup).mapValues(joinValue -> {
+                int initSup = joinValue._2[0];
+                int prevSup = initSup + 1 - joinValue._2.length;
+                if (!joinValue._1.isPresent()) {
+                    if (prevSup < minSup) {
+                        return null;
+                    }
+                    return joinValue._2;
                 }
 
-                for (Integer u : values._1.get()) {
-                    sup -= 1;
+                IntSortedSet invalidVertexes = new IntAVLTreeSet(joinValue._2, 1, joinValue._2.length - 1);
+                for (int u : joinValue._1.get()) {
+                    invalidVertexes.add(u);
                 }
 
-                return sup;
-            }).filter(kv -> kv._2 > 0);
+                int newSup = initSup - invalidVertexes.size();
+                if (newSup == 0)
+                    return null;
+
+                int[] outVal = new int[1 + invalidVertexes.size()];
+                outVal[0] = initSup;
+                System.arraycopy(invalidVertexes.toIntArray(), 0, outVal, 1, outVal.length - 1);
+                return outVal;
+            }).filter(kv -> kv._2 != null).cache();
+
+            prevEdgeSup.unpersist();
         }
 
-        System.out.println("count: " + edgeSup.count());
+        long tEnd = System.currentTimeMillis();
+        System.out.println("count: " + edgeSup.count() + ", duration: " + (tEnd - tStart) + " ms");
         sc.close();
     }
 }
