313750ef473cb5ff7bab15eb04d36ca330fd8a35
mehdi alemi <mehdi.alemi.se@gmail.com>
2017-02-24 12:21:51 +0330

    flink2

--- src/main/java/ir/ac/sbu/graph/flink/KTrussFlink2.java
+++ src/main/java/ir/ac/sbu/graph/flink/KTrussFlink2.java
@@ -5,7 +5,9 @@ import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
 import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
-import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.functions.CoGroupFunction;
+import org.apache.flink.api.common.functions.FlatJoinFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.java.ExecutionEnvironment;
@@ -15,9 +17,7 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.util.Collector;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class KTrussFlink2 {
 
@@ -38,7 +38,7 @@ public class KTrussFlink2 {
             inputPath = args[0];
 
         env.getConfig().enableForceKryo();
-        env.getConfig().enableObjectReuse();
+//        env.getConfig().enableObjectReuse();
         env.getConfig().registerPojoType(int[].class);
 
         long startTime = System.currentTimeMillis();
@@ -75,6 +75,7 @@ public class KTrussFlink2 {
                 .groupBy(0).reduceGroup(new GroupReduceFunction<Tuple3<Integer, Integer, Integer>, Tuple2<Integer, int[]>>() {
                 final List<Tuple3<Integer, Integer, Integer>> list = new ArrayList<>();
                 final List<Tuple3<Integer, Integer, Integer>> holds = new ArrayList<>();
+
                 @Override
                 public void reduce(Iterable<Tuple3<Integer, Integer, Integer>> values, Collector<Tuple2<Integer, int[]>> collector)
                     throws Exception {
@@ -84,18 +85,14 @@ public class KTrussFlink2 {
                         list.add(value);
                     }
 
-                    if (list.size() == 0)
-                        return;
-
                     int v = list.get(0).f0;
                     int deg = list.size();
                     holds.clear();
 
-                    for (int i = 0; i < list.size(); i++) {
+                    for (int i = 0; i < deg; i++) {
                         Tuple3<Integer, Integer, Integer> tuple = list.get(i);
                         if (tuple.f2 > deg || (tuple.f2 == deg && tuple.f1 > tuple.f0))
-                            continue;
-                        holds.add(tuple);
+                            holds.add(tuple);
                     }
 
                     Collections.sort(holds, (a, b) -> a.f2 != b.f2 ? a.f2 - b.f2 : a.f1 - b.f1);
@@ -109,8 +106,83 @@ public class KTrussFlink2 {
                 }
             }).returns(TUPLE_2_INT_ARRAY_TYPE_HINT);
 
-        long count = fonls.count();
+        FlatMapOperator<Tuple2<Integer, int[]>, Tuple2<Integer, int[]>> candidates = fonls.filter(t -> t.f1.length > 2)
+            .flatMap(new FlatMapFunction<Tuple2<Integer, int[]>, Tuple2<Integer, int[]>>() {
+                @Override
+                public void flatMap(Tuple2<Integer, int[]> t, Collector<Tuple2<Integer, int[]>> collector) throws Exception {
+                    int size = t.f1.length - 1;
+                    if (size == 1)
+                        return;
+                    for (int index = 1; index < size; index++) {
+                        int len = size - index;
+                        int[] cvalue = new int[len + 1];
+                        cvalue[0] = t.f0; // First vertex in the triangle
+                        System.arraycopy(t.f1, index + 1, cvalue, 1, len);
+                        Arrays.sort(cvalue, 1, cvalue.length); // quickSort to comfort with fonl
+                        collector.collect(new Tuple2<>(t.f1[index], cvalue));
+                    }
+                }
+            });
+
+        GroupReduceOperator<Tuple3<Integer, Integer, Integer>, Tuple2<Tuple2<Integer, Integer>, int[]>> edgeVertices = fonls.map(t -> {
+            Arrays.sort(t.f1, 1, t.f1.length);
+            return t;
+        }).returns(TUPLE_2_INT_ARRAY_TYPE_HINT).join(candidates)
+            .where(0)
+            .equalTo(0)
+            .with((FlatJoinFunction<Tuple2<Integer, int[]>, Tuple2<Integer, int[]>, Tuple3<Integer, Integer, Integer>>)
+                (first, second, collector) -> {
+                    int[] fonl = first.f1;
+                    int[] can = second.f1;
+                    int v = first.f0;
+                    int u = can[0];
+                    Tuple2<Integer, Integer> uv;
+                    if (u < v)
+                        uv = new Tuple2<>(u, v);
+                    else
+                        uv = new Tuple2<>(v, u);
+
+                    // The intersection determines triangles which u and v are two of their vertices.
+                    // Always generate and edge (u, v) such that u < v.
+                    int fi = 1;
+                    int ci = 1;
+                    while (fi < fonl.length && ci < can.length) {
+                        if (fonl[fi] < can[ci])
+                            fi++;
+                        else if (fonl[fi] > can[ci])
+                            ci++;
+                        else {
+                            int w = fonl[fi];
+                            collector.collect(new Tuple3<>(uv.f0, uv.f1, w));
+                            if (u < w)
+                                collector.collect(new Tuple3<>(u, w, v));
+                            else
+                                collector.collect(new Tuple3<>(w, u, v));
+
+                            if (v < w)
+                                collector.collect(new Tuple3<>(v, w, u));
+                            else
+                                collector.collect(new Tuple3<>(w, v, u));
+                            fi++;
+                            ci++;
+                        }
+                    }
+                }).returns(TUPLE_3_TYPE_HINT).groupBy(0, 1).reduceGroup((Iterable<Tuple3<Integer, Integer, Integer>> values,
+                                       Collector<Tuple2<Tuple2<Integer, Integer>, int[]>> collector) -> {
+                        IntList list = new IntArrayList();
+
+                        Tuple2<Integer, Integer> key = null;
+                        for (Tuple3<Integer, Integer, Integer> i : values) {
+                            if (key == null) {
+                                key = new Tuple2<>(i.f0, i.f1);
+                            }
+                            list.add(i.f2.intValue());
+                        }
+                        collector.collect(new Tuple2<>(key, list.toIntArray()));
+                }).returns(new TypeHint<Tuple2<Tuple2<Integer, Integer>, int[]>>() {});
+
+        long count = edgeVertices.count();
         long endTime = System.currentTimeMillis();
-        System.out.println("fonl count: " + count + ", duration: " + (endTime - startTime));
+        System.out.println("edgeVertices count: " + count + ", duration: " + (endTime - startTime));
     }
 }
