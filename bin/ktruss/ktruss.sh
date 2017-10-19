1251b23ef80f5f8ab553ef0b1e555f759f5b6bc3
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2017-10-19 22:35:14 +0330

    bash

--- bin/ktruss/ktruss.sh
+++ bin/ktruss/ktruss.sh
@@ -2,7 +2,7 @@
 
 CONF_FILE="$PWD/bin/ktruss/ktruss.conf"
 
-bin/run-spark.sh "ir.ac.sbu.graph.ktruss.spark.KTrussSparkTriangleSet" $CONF_FILE 4
+
 bin/run-spark.sh "ir.ac.sbu.graph.ktruss.spark.KTrussSpark" $CONF_FILE 4
-bin/run-spark.sh "ir.ac.sbu.graph.ktruss.KTrussPregel" $CONF_FILE 4
-bin/run-spark.sh "ir.ac.sbu.graph.ktruss.spark.Cohen" $CONF_FILE 4
+#bin/run-spark.sh "ir.ac.sbu.graph.ktruss.KTrussPregel" $CONF_FILE 4
+#bin/run-spark.sh "ir.ac.sbu.graph.ktruss.spark.Cohen" $CONF_FILE 4
