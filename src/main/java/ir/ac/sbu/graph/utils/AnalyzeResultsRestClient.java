91583d0ba8d65825fe314f038a16aface6af7220
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2017-10-26 05:38:35 +0330

    rest client

--- src/main/java/ir/ac/sbu/graph/utils/AnalyzeResultsRestClient.java
+++ src/main/java/ir/ac/sbu/graph/utils/AnalyzeResultsRestClient.java
@@ -1,5 +1,6 @@
 package ir.ac.sbu.graph.utils;
 
+import com.google.gson.Gson;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -8,20 +9,23 @@ import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 
-/**
- * Created by mehdi on 10/26/17.
- */
 public class AnalyzeResultsRestClient {
 
     public static void main(String[] args) throws IOException {
         CloseableHttpClient client = HttpClients.createDefault();
         String hostname = args[0];
         String url = "http://" + hostname + ":18080/api/v1/";
-
-        HttpGet getApp = new HttpGet(url + "/applications");
+        String applicationUrl = url + "applications";
+        HttpGet getApp = new HttpGet(applicationUrl);
         CloseableHttpResponse response = client.execute(getApp);
         String json = EntityUtils.toString(response.getEntity());
-        System.out.println(json);
+        Gson gson = new Gson();
+        Application[] applications = gson.fromJson(json, Application[].class);
+        System.out.println("application num: " + applications.length);
+        for (Application application : applications) {
+            System.out.println("jobId: " + application.getId());
+        }
+        client.close();
     }
 
 
