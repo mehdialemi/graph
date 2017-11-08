c8e556bf7319c40fed48e41f6f2bbab884cc21d2
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2017-11-08 04:05:37 +0330

    bash update

--- bin/kcore/kcore.sh
+++ bin/kcore/kcore.sh
@@ -3,5 +3,7 @@
 CONF_FILE="$PWD/bin/kcore/kcore.conf"
 
 #bin/run-spark.sh "ir.ac.sbu.graph.kcore.KCoreDegInfo" $CONF_FILE 4
-bin/run-spark.sh "ir.ac.sbu.graph.kcore.KCoreNeighborList" $CONF_FILE 4
-bin/run-spark.sh "ir.ac.sbu.graph.spark.KCore" $CONF_FILE 4
+for k in {4,10,20,40,80,160}
+do
+    bin/run-spark.sh "ir.ac.sbu.graph.spark.KCore" $CONF_FILE $k 100
+done
--- bin/ktruss/ktruss.sh
+++ bin/ktruss/ktruss.sh
@@ -3,8 +3,10 @@
 CONF_FILE="$PWD/bin/ktruss/ktruss.conf"
 
 
-#bin/run-spark.sh "ir.ac.sbu.graph.ktruss.spark.KTrussSpark" $CONF_FILE 4
-#bin/run-spark.sh "ir.ac.sbu.graph.spark.KTrussTSet2" $CONF_FILE 3 0 4
-bin/run-spark.sh "ir.ac.sbu.graph.spark.KTrussTSet" $CONF_FILE 0 4 100
-#bin/run-spark.sh "ir.ac.sbu.graph.ktruss.KTrussPregel" $CONF_FILE 4
-#bin/run-spark.sh "ir.ac.sbu.graph.ktruss.spark.Cohen" $CONF_FILE 4
+for k in {4,10,20,40,80,160}
+do
+    for kci in {0,1,2,3,4}
+    do
+        bin/run-spark.sh "ir.ac.sbu.graph.spark.KTrussTSet" $CONF_FILE $kci $k 100
+    done
+done
