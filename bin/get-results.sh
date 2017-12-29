b89b3af5b9c5361e2935c6287e15bf2bfc7b6b08
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2017-12-29 15:55:51 +0330

    get result by app id

--- bin/get-results.sh
+++ bin/get-results.sh
@@ -28,4 +28,4 @@ if [ -z "$1" ]
     app=$1
 fi
 
-java -cp ${config[jar]} ir.ac.sbu.graph.utils.AnalyzeResultsRestClient ${config[host]} logs/${config[app]}-results/${input} ${config[command]} ${config[appId]} ${config[limit]}
+java -cp ${config[jar]} ir.ac.sbu.graph.utils.AnalyzeResultsRestClient ${config[host]} logs/${config[app]}-results ${config[command]} ${config[appId]} ${config[limit]}
