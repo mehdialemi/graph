6ecf4d209c75091e07fd4007b4530148fd1e69f7
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2017-12-29 15:49:24 +0330

    get result by app id

--- src/main/java/ir/ac/sbu/graph/utils/AnalyzeResultsRestClient.java
+++ src/main/java/ir/ac/sbu/graph/utils/AnalyzeResultsRestClient.java
@@ -33,6 +33,7 @@ public class AnalyzeResultsRestClient {
         System.out.println("application num: " + applications.length);
         for (Application application : applications) {
 
+            System.out.println("Application id: " + application.getId());
             if (command.equals(BATCH_COMMAND) || appId.equals(application.getId()) ) {
                 AnalyzeAppResults analyzeAppResults = new AnalyzeAppResults(url, client, application, outputDir);
                 analyzeAppResults.start();
