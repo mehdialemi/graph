934e02721eb8df25ba739308ec47ac3bc9ed4929
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-03-17 01:18:22 +0330

    EdgeListUndirector

--- src/main/java/ir/ac/sbu/graph/utils/EdgeListUndirector.java
+++ src/main/java/ir/ac/sbu/graph/utils/EdgeListUndirector.java
@@ -24,7 +24,8 @@ public class EdgeListUndirector extends SparkApp  {
         JavaRDD<String> input = conf.getSc().textFile(conf.getInputPath());
         JavaRDD<String> edges = input.filter(l -> !l.startsWith("#"))
                 .map(s -> s.split("\\s+"))
-                .flatMap(array -> Arrays.asList(array[0] + "\t" + array[1], array[1] + "\t" + array[0]).iterator());
+                .flatMap(array -> Arrays.asList(array[0] + "\t" + array[1], array[1] + "\t" + array[0]).iterator())
+                .distinct();
 
         String outPath = conf.getInputPath() + ".edges";
         System.out.println("Deleting " + outPath);
