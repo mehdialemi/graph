c0c40621abaeb6d7d57f34317f7db8599d97b4af
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-03-19 05:59:24 +0330

    EdgeListUndirector Parallel

--- src/main/java/ir/ac/sbu/graph/utils/EdgeListUndirector.java
+++ src/main/java/ir/ac/sbu/graph/utils/EdgeListUndirector.java
@@ -1,48 +1,53 @@
 package ir.ac.sbu.graph.utils;
 
-import ir.ac.sbu.graph.spark.ArgumentReader;
-import ir.ac.sbu.graph.spark.SparkApp;
-import ir.ac.sbu.graph.spark.SparkAppConf;
+import ir.ac.sbu.graph.spark.*;
 import org.apache.commons.io.FileUtils;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  *
  */
 public class EdgeListUndirector extends SparkApp  {
 
-    public EdgeListUndirector(SparkAppConf conf) {
-        super(conf);
+    private NeighborList neighborList;
+
+    public EdgeListUndirector(NeighborList neighborList) {
+        super(neighborList);
+        this.neighborList = neighborList;
     }
 
-    public void generate() throws FileNotFoundException {
-        JavaRDD<String> input = conf.getSc().textFile(conf.getInputPath());
-        JavaRDD<String> edges = input.filter(l -> !l.startsWith("#"))
-                .map(s -> s.split("\\s+"))
-                .flatMap(array -> Arrays.asList(array[0] + "\t" + array[1], array[1] + "\t" + array[0]).iterator())
-                .distinct();
-
+    public void generate() {
         String outPath = conf.getInputPath() + ".edges";
         System.out.println("Deleting " + outPath);
         FileUtils.deleteQuietly(new File(outPath));
         System.out.println("New vertex edge list file in " + outPath);
-        PrintWriter pw = new PrintWriter(new File(outPath));
-        for (String edge : edges.collect()) {
-            pw.println(edge);
-            pw.flush();
-        }
-        pw.flush();
+
+        JavaPairRDD<Integer, int[]> neighbors = neighborList.getOrCreate();
+        JavaRDD<String> edges = neighbors.flatMap(kv -> {
+            List<String> out = new ArrayList<>();
+            int v1 = kv._1.intValue();
+            for (int v2 : kv._2) {
+                out.add(v1 + "\t" + v2);
+            }
+            return out.iterator();
+        });
+
+        edges.saveAsTextFile(outPath);
+        conf.getSc().close();
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         SparkAppConf conf = new SparkAppConf(new ArgumentReader(args));
         conf.init();
-        EdgeListUndirector eToV = new EdgeListUndirector(conf);
+        EdgeListUndirector eToV = new EdgeListUndirector(new NeighborList(new EdgeLoader(conf)));
         eToV.generate();
     }
 }
