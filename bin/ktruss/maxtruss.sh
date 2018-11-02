68c28d23936995e8a7553216e5e4e1a276cf75db
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-11-02 15:16:42 +0330

    always create tset array in update

--- bin/ktruss/maxtruss.sh
+++ bin/ktruss/maxtruss.sh
@@ -3,5 +3,5 @@
 CONF_FILE="$PWD/bin/ktruss/ktruss.conf"
 
 
-bin/run-spark.sh "ir.ac.sbu.graph.spark.ktruss.MaxKTrussTSet" $CONF_FILE 120 70 20
+bin/run-spark.sh "ir.ac.sbu.graph.spark.ktruss.MaxKTrussTSet" $CONF_FILE 120 100 20
 #bin/run-spark.sh "ir.ac.sbu.graph.spark.ktruss.MaxKTrussTSetPartialUpdate" $CONF_FILE 120
