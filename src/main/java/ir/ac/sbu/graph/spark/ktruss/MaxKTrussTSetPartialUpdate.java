848690d641409c0e2262120dcfed705dffce85a3
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-11-02 07:49:49 +0330

    correct MaxKTrussTSetPartialUpdate

--- src/main/java/ir/ac/sbu/graph/spark/ktruss/MaxKTrussTSetPartialUpdate.java
+++ src/main/java/ir/ac/sbu/graph/spark/ktruss/MaxKTrussTSetPartialUpdate.java
@@ -55,20 +55,20 @@ public class MaxKTrussTSetPartialUpdate extends SparkApp {
         this.conf.getSc().setCheckpointDir("hdfs://" + masterHost + "/shared/checkpoint");
     }
 
-    public Map<Integer, JavaRDD<Edge>> explore() {
+    public Map <Integer, JavaRDD <Edge>> explore() {
         KCore kCore = new KCore(neighborList, kCoreConf);
 
         Triangle triangle = new Triangle(kCore);
 
-        JavaPairRDD<Integer, int[]> fonl = triangle.getOrCreateFonl();
+        JavaPairRDD <Integer, int[]> fonl = triangle.getOrCreateFonl();
 
-        JavaPairRDD<Integer, int[]> candidates = triangle.createCandidates(fonl);
+        JavaPairRDD <Integer, int[]> candidates = triangle.createCandidates(fonl);
 
         int partitionNum = fonl.getNumPartitions();
 
-        JavaPairRDD<Edge, int[]> tSet = createTSet(fonl, candidates);
+        JavaPairRDD <Edge, int[]> tSet = createTSet(fonl, candidates);
 
-        Map<Integer, JavaRDD<Edge>> eTrussMap = new HashMap <>();
+        Map <Integer, JavaRDD <Edge>> eTrussMap = new HashMap <>();
 
         int k = 4;
         long count = tSet.count();
@@ -82,7 +82,7 @@ public class MaxKTrussTSetPartialUpdate extends SparkApp {
             final int minSup = k - 2;
             final int maxSup = minSup + 5;
 
-            JavaRDD<Edge> kTruss = conf.getSc().parallelize(new ArrayList <>());
+            JavaRDD <Edge> kTruss = conf.getSc().parallelize(new ArrayList <>());
             long kTrussCount = 0;
             while (true) {
                 JavaPairRDD <Edge, int[]> subTSet = tSet.filter(kv -> kv._2[0] < maxSup).cache();
@@ -113,13 +113,13 @@ public class MaxKTrussTSetPartialUpdate extends SparkApp {
             eTrussCount += kTrussCount;
             long tSetCount = tSet.count();
             long totalCount = tSetCount + eTrussCount;
-            log("minSup = " + minSup + ", kTrussCount: " + kTrussCount+
-                    ", eTrussCount: " + eTrussCount + ", tSetCount: " + tSetCount + ", total count: " + totalCount,
+            log("minSup = " + minSup + ", kTrussCount: " + kTrussCount +
+                            ", eTrussCount: " + eTrussCount + ", tSetCount: " + tSetCount + ", total count: " + totalCount,
                     t1, t2);
 
             if (tSetCount == 0)
                 break;
-            k ++;
+            k++;
         }
         return eTrussMap;
     }
@@ -128,29 +128,29 @@ public class MaxKTrussTSetPartialUpdate extends SparkApp {
         return tSet.filter(kv -> kv._2[0] >= minSup)
                 .leftOuterJoin(tSetUpdate)
                 .mapValues(v -> {
-            if (!v._2.isPresent() || v._1[0] < maxSup)
-                return v._1;
+                    if (!v._2.isPresent() || v._1[0] < maxSup)
+                        return v._1;
 
-            int[] update = v._2.get();
-            IntSet iSet = new IntOpenHashSet();
-            for (int u : update) {
-                iSet.add(u);
-            }
+                    int[] update = v._2.get();
+                    IntSet iSet = new IntOpenHashSet();
+                    for (int i = 1; i < update.length; i++) {
+                        iSet.add(update[i]);
+                    }
 
-            int[] set = v._1;
-            for (int i = META_LEN; i < set.length; i++) {
-                if (set[i] == INVALID)
-                    continue;
-                if (iSet.contains(set[i])) {
-                    set[0]--;
-                    set[i] = INVALID;
-                }
-            }
-            return set;
-        }).cache();
+                    int[] set = v._1;
+                    for (int i = META_LEN; i < set.length; i++) {
+                        if (set[i] == INVALID)
+                            continue;
+                        if (iSet.contains(set[i])) {
+                            set[0]--;
+                            set[i] = INVALID;
+                        }
+                    }
+                    return set;
+                }).cache();
     }
 
-    private  JavaPairRDD<Edge, int[]> invalidByKCore( JavaPairRDD<Edge, int[]> tSet, int kc) {
+    private JavaPairRDD <Edge, int[]> invalidByKCore(JavaPairRDD <Edge, int[]> tSet, int kc) {
         kCoreConf.setKcMaxIter(Integer.MAX_VALUE);
         kCoreConf.setKc(kc);
         KCore kCore = new KCore(tSet.keys(), kCoreConf);
@@ -167,17 +167,17 @@ public class MaxKTrussTSetPartialUpdate extends SparkApp {
         return tSet.subtractByKey(kCoreEdges);
     }
 
-    public Tuple2<JavaRDD<Edge>, JavaPairRDD<Edge, int[]>> generate(
-            int minSup, JavaPairRDD<Edge, int[]> tSet, int partitionNum) {
+    public Tuple2 <JavaRDD <Edge>, JavaPairRDD <Edge, int[]>> generate(
+            int minSup, JavaPairRDD <Edge, int[]> tSet, int partitionNum) {
 
-        JavaRDD<Edge> eTruss = conf.getSc().parallelize(new ArrayList <>());
+        JavaRDD <Edge> eTruss = conf.getSc().parallelize(new ArrayList <>());
 
-        Queue<JavaPairRDD<Edge, int[]>> tSetQueue = new LinkedList<>();
+        Queue <JavaPairRDD <Edge, int[]>> tSetQueue = new LinkedList <>();
         tSetQueue.add(tSet);
 
         int iter = 0;
-        while(true) {
-            iter ++;
+        while (true) {
+            iter++;
             long t1 = System.currentTimeMillis();
             iterations.incrementAndGet();
 
@@ -187,7 +187,7 @@ public class MaxKTrussTSetPartialUpdate extends SparkApp {
 
                 log("check pointing tSet", t, System.currentTimeMillis());
             }
-            JavaPairRDD<Edge, int[]> invalids = tSet.filter(kv -> kv._2[0] < minSup && kv._2[0] >= 0).cache();
+            JavaPairRDD <Edge, int[]> invalids = tSet.filter(kv -> kv._2[0] < minSup && kv._2[0] >= 0).cache();
             long invalidCount = invalids.count();
 
             // If no invalid edge is found then the program terminates
@@ -206,30 +206,30 @@ public class MaxKTrussTSetPartialUpdate extends SparkApp {
             // The edges in the key part of invalids key-values should be removed. So, we detect other
             // edges of their involved triangle from their triangle vertex set. Here, we determine the
             // vertices which should be removed from the triangle vertex set related to the other edges.
-            JavaPairRDD<Edge, Iterable<Integer>> invUpdates = invalids.flatMapToPair(kv -> {
+            JavaPairRDD <Edge, Iterable <Integer>> invUpdates = invalids.flatMapToPair(kv -> {
                 int i = META_LEN;
 
                 Edge e = kv._1;
-                List<Tuple2<Edge, Integer>> out = new ArrayList<>((kv._2.length - 3) * 2);
+                List <Tuple2 <Edge, Integer>> out = new ArrayList <>((kv._2.length - 3) * 2);
                 for (; i < kv._2[1]; i++) {
                     if (kv._2[i] == INVALID)
                         continue;
-                    out.add(new Tuple2<>(new Edge(e.v1, kv._2[i]), e.v2));
-                    out.add(new Tuple2<>(new Edge(e.v2, kv._2[i]), e.v1));
+                    out.add(new Tuple2 <>(new Edge(e.v1, kv._2[i]), e.v2));
+                    out.add(new Tuple2 <>(new Edge(e.v2, kv._2[i]), e.v1));
                 }
 
                 for (; i < kv._2[2]; i++) {
                     if (kv._2[i] == INVALID)
                         continue;
-                    out.add(new Tuple2<>(new Edge(e.v1, kv._2[i]), e.v2));
-                    out.add(new Tuple2<>(new Edge(kv._2[i], e.v2), e.v1));
+                    out.add(new Tuple2 <>(new Edge(e.v1, kv._2[i]), e.v2));
+                    out.add(new Tuple2 <>(new Edge(kv._2[i], e.v2), e.v1));
                 }
 
                 for (; i < kv._2[3]; i++) {
                     if (kv._2[i] == INVALID)
                         continue;
-                    out.add(new Tuple2<>(new Edge(kv._2[i], e.v1), e.v2));
-                    out.add(new Tuple2<>(new Edge(kv._2[i], e.v2), e.v1));
+                    out.add(new Tuple2 <>(new Edge(kv._2[i], e.v1), e.v2));
+                    out.add(new Tuple2 <>(new Edge(kv._2[i], e.v2), e.v1));
                 }
 
                 return out.iterator();
@@ -288,58 +288,58 @@ public class MaxKTrussTSetPartialUpdate extends SparkApp {
             tSetQueue.add(tSet);
         }
 
-        return new Tuple2<>(eTruss, tSet);
+        return new Tuple2 <>(eTruss, tSet);
     }
 
-    private JavaPairRDD<Edge, int[]> createTSet(JavaPairRDD<Integer, int[]> fonl,
-                                                JavaPairRDD<Integer, int[]> candidates) {
+    private JavaPairRDD <Edge, int[]> createTSet(JavaPairRDD <Integer, int[]> fonl,
+                                                 JavaPairRDD <Integer, int[]> candidates) {
         int partitionNum = fonl.getNumPartitions();
         // Generate kv such that key is an edge and value is its triangle vertices.
-        JavaPairRDD<Edge, int[]> tSet = candidates.cogroup(fonl)
+        JavaPairRDD <Edge, int[]> tSet = candidates.cogroup(fonl)
                 .flatMapToPair(t -> {
-            int[] fVal = t._2._2.iterator().next();
-            Arrays.sort(fVal, 1, fVal.length);
-            int v = t._1;
+                    int[] fVal = t._2._2.iterator().next();
+                    Arrays.sort(fVal, 1, fVal.length);
+                    int v = t._1;
 
-            List<Tuple2<Edge, VertexByte>> output = new ArrayList<>();
-            for (int[] cVal : t._2._1) {
-                int u = cVal[0];
-                Edge uv = new Edge(u, v);
+                    List <Tuple2 <Edge, VertexByte>> output = new ArrayList <>();
+                    for (int[] cVal : t._2._1) {
+                        int u = cVal[0];
+                        Edge uv = new Edge(u, v);
 
-                // The intersection determines triangles which u and v are two of their vertices.
-                // Always generate and edge (u, v) such that u < v.
-                int fi = 1;
-                int ci = 1;
-                while (fi < fVal.length && ci < cVal.length) {
-                    if (fVal[fi] < cVal[ci])
-                        fi++;
-                    else if (fVal[fi] > cVal[ci])
-                        ci++;
-                    else {
-                        int w = fVal[fi];
-                        Edge uw = new Edge(u, w);
-                        Edge vw = new Edge(v, w);
+                        // The intersection determines triangles which u and v are two of their vertices.
+                        // Always generate and edge (u, v) such that u < v.
+                        int fi = 1;
+                        int ci = 1;
+                        while (fi < fVal.length && ci < cVal.length) {
+                            if (fVal[fi] < cVal[ci])
+                                fi++;
+                            else if (fVal[fi] > cVal[ci])
+                                ci++;
+                            else {
+                                int w = fVal[fi];
+                                Edge uw = new Edge(u, w);
+                                Edge vw = new Edge(v, w);
 
-                        output.add(new Tuple2<>(uv, new VertexByte(w, W_UVW)));
-                        output.add(new Tuple2<>(uw, new VertexByte(v, V_UVW)));
-                        output.add(new Tuple2<>(vw, new VertexByte(u, U_UVW)));
+                                output.add(new Tuple2 <>(uv, new VertexByte(w, W_UVW)));
+                                output.add(new Tuple2 <>(uw, new VertexByte(v, V_UVW)));
+                                output.add(new Tuple2 <>(vw, new VertexByte(u, U_UVW)));
 
-                        fi++;
-                        ci++;
+                                fi++;
+                                ci++;
+                            }
+                        }
                     }
-                }
-            }
 
-            return output.iterator();
-        }).groupByKey(partitionNum * 2)
+                    return output.iterator();
+                }).groupByKey(partitionNum * 2)
                 .mapValues(values -> {
-                    List<VertexByte> list = new ArrayList<>();
+                    List <VertexByte> list = new ArrayList <>();
                     int sw = 0, sv = 0, su = 0;
                     for (VertexByte value : values) {
                         list.add(value);
                         if (value.b == W_UVW)
                             sw++;
-                        else if (value.b  == V_UVW)
+                        else if (value.b == V_UVW)
                             sv++;
                         else
                             su++;
