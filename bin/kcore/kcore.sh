685cb14873d9bcb399ab9754df0a2adc1ef83574
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-12-10 05:51:53 +0330

    kcore script

--- bin/kcore/kcore.sh
+++ bin/kcore/kcore.sh
@@ -2,45 +2,30 @@
 
 CONF_FILE="$PWD/bin/kcore/kcore.conf"
 
-#bin/run-spark.sh "ir.ac.sbu.graph.kcore.KCoreDegInfo" $CONF_FILE 4
-#for cores in {12..12}
-#do
+function kcore {
+
+    kValues=("$@")
+    for k in "${kValues[@]}"
+    do
+            bin/run-spark.sh "ir.ac.sbu.graph.spark.kcore.KCore" $CONF_FILE 120 $k 1000
+    done
+}
+
 echo "youtube" > bin/inputs
-for k in {4,40,51}
-do
-    bin/run-spark.sh "ir.ac.sbu.graph.spark.kcore.KCore" $CONF_FILE 120 $k 1000
-done
+kcore {4,40,51}
 
 echo "cit-patents" > bin/inputs
-for k in {4,40,64}
-do
-    bin/run-spark.sh "ir.ac.sbu.graph.spark.kcore.KCore" $CONF_FILE 120 $k 1000
-done
+kcore {4,40,64}
 
 echo "soc-liveJournal" > bin/inputs
-for k in {4,40,80,160,372}
-do
-    bin/run-spark.sh "ir.ac.sbu.graph.spark.kcore.KCore" $CONF_FILE 120 $k 1000
-done
+kcore {4,40,80,160,372}
 
 echo "orkut" > bin/inputs
-for k in {4,40,80,160,253}
-do
-    bin/run-spark.sh "ir.ac.sbu.graph.spark.kcore.KCore" $CONF_FILE 120 $k 1000
-done
+kcore {4,40,80,160,253}
 
 echo "friendster" > bin/inputs
-for k in {4,40,80,160,304}
-do
-    bin/run-spark.sh "ir.ac.sbu.graph.spark.kcore.KCore" $CONF_FILE 120 $k 1000
-done
+kcore {4,40,80,160,304}
 
 echo "twitter" > bin/inputs
-for k in {4,40,80,160,2647}
-do
-    bin/run-spark.sh "ir.ac.sbu.graph.spark.kcore.KCore" $CONF_FILE 120 $k 1000
-done
-
-
-
+kcore {4,40,80,160,2647}
 #done
