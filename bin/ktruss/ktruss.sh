5263409c7bf0bece3b93031adca47e44a476f097
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-12-10 06:42:11 +0330

    ktruss script

--- bin/ktruss/ktruss.sh
+++ bin/ktruss/ktruss.sh
@@ -2,14 +2,28 @@
 
 CONF_FILE="$PWD/bin/ktruss/ktruss.conf"
 
-
-for cores in {12..12}
-do
-    for k in {4,10,20,40,80,160}
+function ktruss {
+    kValues=("$@")
+    for k in "${kValues[@]}"
     do
-        for kci in {0,1,2,3,4}
-        do
-            bin/run-spark.sh "ir.ac.sbu.graph.spark.ktruss.KTrussTSet" $CONF_FILE $cores $kci $k 100
-        done
+        bin/run-spark.sh "ir.ac.sbu.graph.spark.ktruss.KTrussTSet" $CONF_FILE 120 1000 $k 100
     done
-done
+}
+
+echo "youtube" > bin/inputs
+ktruss {4,17}
+
+echo "cit-patents" > bin/inputs
+ktruss {4,36}
+
+echo "soc-liveJournal" > bin/inputs
+ktruss {4,40,80,160,362}
+
+echo "orkut" > bin/inputs
+ktruss {4,40,75}
+
+echo "friendster" > bin/inputs
+ktruss {4,40,129}
+
+echo "twitter" > bin/inputs
+ktruss {4,40,80,160,1998}
