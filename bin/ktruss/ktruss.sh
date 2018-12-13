0033625cd7158c8796ee49120ca432d0699106a4
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-12-14 00:29:17 +0330

    change partitions

--- bin/ktruss/ktruss.sh
+++ bin/ktruss/ktruss.sh
@@ -8,20 +8,20 @@ function ktruss {
     bin/run-spark.sh "ir.ac.sbu.graph.spark.ktruss.KTrussTSet" $CONF_FILE 120 $p 5 $k 1000
     bin/run-spark.sh "ir.ac.sbu.graph.spark.ktruss.KTrussTSet" $CONF_FILE 120 $p 0 $k 1000
 }
-echo "youtube" > bin/inputs
-ktruss 19 20
-#
-echo "cit-patents" > bin/inputs
-ktruss 36 34
-#
-echo "soc-liveJournal" > bin/inputs
-ktruss  362 132
-#
-echo "orkut" > bin/inputs
-ktruss 78  159
-#
+#echo "youtube" > bin/inputs
+#ktruss 19 20
+##
+#echo "cit-patents" > bin/inputs
+#ktruss 36 34
+##
+#echo "soc-liveJournal" > bin/inputs
+#ktruss  362 132
+##
+#echo "orkut" > bin/inputs
+#ktruss 78  159
+##
 echo "friendster" > bin/inputs
 ktruss 39   492
-
-echo "twitter" > bin/inputs
-ktruss 1998 1157
+#
+#echo "twitter" > bin/inputs
+#ktruss 1998 1157
