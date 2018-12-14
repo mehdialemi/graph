dec7f91a084f8130de8558f50067f95693d14649
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2018-12-14 12:56:01 +0330

    p 5

--- src/main/java/ir/ac/sbu/graph/spark/ktruss/KTrussTSet.java
+++ src/main/java/ir/ac/sbu/graph/spark/ktruss/KTrussTSet.java
@@ -206,7 +206,7 @@ public class KTrussTSet extends SparkApp {
                 });
 
         // Generate kv such that key is an edge and value is its triangle vertices.
-        return fonl.cogroup(candidates, conf.getPartitionNum() * 2)
+        return fonl.cogroup(candidates, conf.getPartitionNum() * 5)
                 .flatMapToPair(t -> {
 
                     int[] fVal = t._2._1.iterator().next();
