0a80cc132cb788b752170c5f16546c925442e0ae
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2019-07-26 11:15:38 +0430

    local config

--- bin/pattern/run-spark.sh
+++ bin/pattern/run-spark.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 
-CONF_FILE="$PWD/bin/ktruss/ktruss.conf"
+CONF_FILE="$PWD/bin/pattern/config.yaml"
 MAIN_CLASS="ir.ac.sbu.graph.spark.search.PatternCounter"
-SPARK_HOME=""
 SPARK_MASTER="alemi-1"
 JAR_FILE="$PWD/target/subgraph-mining-1.0-jar-with-dependencies.jar"
-LOG_DIR="logs/pattern/"
-
+LOG_DIR="logs/pattern"
 
 echo "Running search with graph: $graph, pattern: $pattern"
-
-command="$SPARK_HOME/bin/spark-submit --class $MAIN_CLASS --master $SPARK_MASTER:7077 $JAR_FILE bin/pattern/config.yaml"
 log_name=`date +%Y-%m-%d.%H.%M.%S`
 log_file="$LOG_DIR/$log_name"
 
 echo "Running $command"
-nohup $SPARK_HOME/bin/spark-submit --class $MAIN_CLASS --master spark://$SPARK_MASTER:7077 $JAR_FILE search.config > $log_file 2>&1 &
+nohup ~/spark-2.2/bin/spark-submit --class $MAIN_CLASS --master spark://$SPARK_MASTER:7077 $JAR_FILE $CONF_FILE > $log_file 2>&1 &
 tail -f $log_file
